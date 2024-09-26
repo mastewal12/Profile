77ff2a064cfba403496ea35e835f683a32963bfa
mastewal12 <mastewaltakele8@gmail.com>
2024-09-25 19:08:27 -0500

    profile aded

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
-const Profile = () => {
+const Profile = ({ user }) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="profileCard">
+      <h1>{user.name} </h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        // style={{
+        //     width: user.imageSize,
+        //     height: user.imageSize,
+        //     borderRadius: '50%'
+        // }}
+      />
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
-const User = () => {
+const User = ({ data }) => {
+  const { name, imageSize, imageUrl } = data;
   return (
-    <div className='text-primary'>User</div>
-  )
-}
-
-export default User
+    <div
+      className="shadow-lg"
+      style={{
+        margin: "40px auto",
+        width: "32%",
+        maxWidth: 320,
+        textAlign: "center",
+        backgroundColor: "teal",
+        color: "whitesmoke",
+        borderRadius: "10px",
+        padding: "15px",
+        boxShadow: "0 14px 18px rgba(0, 0, 0, 0.5)",
+      }}
+    >
+      <h1>{name}</h1>
+      <img
+        className="avatar"
+        src={imageUrl}
+        alt={"Photo of " + name}
+        style={{
+          width: imageSize,
+          height: imageSize,
+          borderRadius: "50%",
+        }}
+      />
+    </div>
+  );
+};
+export default User;
